7de93fd5a5b39739335736cdd5694b34bd352525
git <18611822573@163.com>
2016-02-25 15:51:23 +0800

    微信验证请求URL超时，因为忘记传递request，已修改

--- backend/wx/joint.js
+++ backend/wx/joint.js
@@ -17,28 +17,21 @@ router.use((req, res, next) => {
 
 // 定义网站主页的路由
 router.get('/valid/:account_uuid', (req, res) => {
-    console.log("================" + req.params.account_uuid);
     customer.queryCustomerByUUID(req.params.account_uuid).then(row => {
-        valid(row.wx_token);
+        valid(row.wx_token, req, res);
     }, e => {
         console.error(e);
         resUtil.resultFail("系统异常，稍后重试！", req, res);
     })
 });
 
-function valid(wx_token) {
-    console.log("================" + wx_token);
+function valid(wx_token, req, res) {
     let query = url.parse(req.url, true).query;
 
-    console.log("query================" + query);
     let signature = query.signature;
-    console.log("signature================" + signature);
     let echostr = query.echostr;
-    console.log("echostr================" + scyptoString);
     let timestamp = query['timestamp'];
-    console.log("timestamp================" + timestamp);
     let nonce = query.nonce;
-    console.log("nonce================" + nonce);
 
     let oriArray = new Array();
     oriArray[0] = nonce;
@@ -46,17 +39,12 @@ function valid(wx_token) {
     oriArray[2] = wx_token;
     oriArray.sort();
     let original = oriArray.join('');
-    console.log("original================" + original);
     let scyptoString = sha1(original);
 
-    console.log("================" + scyptoString);
-
     if (signature == scyptoString) {
-        console.log("================" + "success");
         res.end(echostr);
         console.log("验证成功并返回了echostr！");
     } else {
-        console.log("================" + "failed");
         res.end("false");
         console.log("验证失败!");
     }
