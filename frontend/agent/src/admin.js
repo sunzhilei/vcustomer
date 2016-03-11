809eeefce3e23fe4b91a34176a7a3a4cd2a977c6
git <18611822573@163.com>
2016-03-11 22:58:30 +0800

    ...

--- frontend/agent/src/admin.js
+++ frontend/agent/src/admin.js
@@ -2,32 +2,8 @@
  * Created by sunzhilei on 2016/1/7.
  */
 
-import { Router, browserHistory } from 'react-router'
-
 import NavComponent from '../../public/lib/Nav';
 import ListGroupComponent from '../../public/lib/ListGroup';
-
-//公共删除组件
-import DelCategoryComponent from './admin/del';
-
-import DockInfoComponent from './admin/personalCenter/DockInfo';
-import DockConfigComponent from './admin/personalCenter/DockConfig';
-
-import CategoryListComponent from './admin/category/list';
-import AddCategoryComponent from './admin/category/add';
-
-import ItemListComponent from './admin/item/list';
-import AddItemComponent from './admin/item/add';
-
-//媒体库
-import FileLibraryComponent from '../../public/lib/FileLibrary';
-class MediaLibraryComponent extends React.Component {
-    render() {
-        return (
-            <FileLibraryComponent modalState={true}/>
-        )
-    }
-}
 
 
 let NavData = {
@@ -59,6 +35,7 @@ class AgentIndex extends React.Component {
                 <div className="container-fluid">
                     <div className="row">
                         <div className="col-sm-3 col-md-2 blog-sidebar">
+                            <ListGroupComponent data={ListGroupData}/>
                         </div>
                         <div className="col-sm-9 col-md-10 blog-main">
                             {this.props.children}
@@ -70,28 +47,4 @@ class AgentIndex extends React.Component {
     }
 }
 
-const routes = {
-    path: '/',
-    component: AgentIndex,
-    childRoutes: [
-        {path: '/admin', component: ''},
-        {path: '/mediaLibrary', component: MediaLibraryComponent},
-
-        {path: '/admin/getDockInfo', component: DockInfoComponent},
-        {path: '/admin/getDockConfig', component: DockConfigComponent},
-
-        {path: '/admin/getCategoryList', component: CategoryListComponent},
-        {path: '/admin/getCategoryInfo', component: AddCategoryComponent},
-        {path: '/admin/editCategoryInfo/:uuid', component: AddCategoryComponent},
-        {path: '/admin/delCategoryInfo', component: DelCategoryComponent},
-
-        {path: '/admin/getItemList', component: ItemListComponent},
-        {path: '/admin/getItemInfo', component: AddItemComponent},
-        {path: '/admin/editItemInfo/:uuid', component: AddItemComponent},
-        {path: '/admin/delItemInfo', component: DelCategoryComponent}
-
-
-    ]
-}
-
-ReactDOM.render(<Router history={browserHistory} routes={routes}/>, document.getElementById('content'));
+ReactDOM.render(<AgentIndex/>, document.getElementById('content'));
