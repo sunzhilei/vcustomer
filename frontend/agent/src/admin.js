bfffe446db5a48f3dcd285627f653d1cf64b674b
git <18611822573@163.com>
2016-03-11 23:14:16 +0800

    ...

--- frontend/agent/src/admin.js
+++ frontend/agent/src/admin.js
@@ -7,6 +7,28 @@ import { Router, browserHistory } from 'react-router'
 import NavComponent from '../../public/lib/Nav';
 import ListGroupComponent from '../../public/lib/ListGroup';
 
+//公共删除组件
+import DelCategoryComponent from './admin/del';
+
+import DockInfoComponent from './admin/personalCenter/DockInfo';
+import DockConfigComponent from './admin/personalCenter/DockConfig';
+
+import CategoryListComponent from './admin/category/list';
+import AddCategoryComponent from './admin/category/add';
+
+import ItemListComponent from './admin/item/list';
+import AddItemComponent from './admin/item/add';
+
+//媒体库
+import FileLibraryComponent from '../../public/lib/FileLibrary';
+class MediaLibraryComponent extends React.Component {
+    render() {
+        return (
+            <FileLibraryComponent modalState={true}/>
+        )
+    }
+}
+
 
 let NavData = {
     brand: {text: '微客', href: '/admin'},
@@ -53,7 +75,21 @@ const routes = {
     path: '/',
     component: AgentIndex,
     childRoutes: [
-        {path: '/admin', component: ''}
+        {path: '/admin', component: ''},
+        {path: '/mediaLibrary', component: MediaLibraryComponent},
+
+        {path: '/admin/getDockInfo', component: DockInfoComponent},
+        {path: '/admin/getDockConfig', component: DockConfigComponent},
+
+        {path: '/admin/getCategoryList', component: CategoryListComponent},
+        {path: '/admin/getCategoryInfo', component: AddCategoryComponent},
+        {path: '/admin/editCategoryInfo/:uuid', component: AddCategoryComponent},
+        {path: '/admin/delCategoryInfo', component: DelCategoryComponent},
+
+        {path: '/admin/getItemList', component: ItemListComponent},
+        {path: '/admin/getItemInfo', component: AddItemComponent},
+        {path: '/admin/editItemInfo/:uuid', component: AddItemComponent},
+        {path: '/admin/delItemInfo', component: DelCategoryComponent}
     ]
 }
 
